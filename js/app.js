32bc1f38fc561f03a8da7ca6ce8a473b749f6604
suerlov <tantian1991@gmail.com>
2016-12-09 19:08:25 +0800

    add project

---
+++ js/app.js
@@ -0,0 +1,159 @@
+'use strict';
+
+// Enemies our player must avoid
+var Enemy = function(y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+
+    this.x = -120;
+    this.y =  y;
+    this.speed = speed;
+
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    this.x += this.speed * dt;
+
+    if (this.x >= 500) {
+        this.resetBug();
+    }
+
+
+};
+
+// resets the enemy location.
+Enemy.prototype.resetBug = function() {
+    this.x = -120;
+};
+
+
+
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// create 3 enemy players.
+var enemy1 = new Enemy(65, Math.floor(Math.random() * 450 + 1));
+var enemy2 = new Enemy(150, Math.floor(Math.random() * 450 + 1));
+var enemy3 = new Enemy(225, Math.floor(Math.random() * 450 + 1));
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+
+// player class
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+
+    this.x = x || 200;
+    this.y = y || 400;
+
+
+};
+
+
+// player's update function. this function does two purposes. first it checks that the player doesn't go off the canvas
+// the second purpose is to call the collision function
+Player.prototype.update = function() {
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+    else if (this.x + 101 > 500) {
+        this.x = 400;
+    }
+    else if (this.y < 0) {
+        this.y = 405;
+    }
+    else if (this.y > 405) {
+        this.y = 405;
+    }
+    this.collide();
+};
+
+// draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// resets the player location. this is basically called when the collision happens with the enemy.
+Player.prototype.resetPlayer = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+
+// handleInput function moves the player depending on the keys pressed by the player.
+Player.prototype.handleInput = function(keycode) {
+    switch(keycode) {
+        case "left":
+            this.x -= 100;
+            break;
+        case "right":
+            this.x += 100;
+            break;
+        case "up":
+            this.y -= 90;
+            break;
+        case "down":
+            this.y += 90;
+            break;
+    }
+};
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
+allEnemies.push(enemy1, enemy2, enemy3);
+
+
+// collide function checks if a collision happens between player and the enemies.
+// if there's a collision then it resets both the player and the enemy.
+Player.prototype.collide = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (this.x < allEnemies[i].x + 50 && this.x + 50 > allEnemies[i].x && this.y < allEnemies[i].y + 30 && this.y + 30 > allEnemies[i].y) {
+            // plays sound on collision
+            document.getElementById('sound-two').play();
+            this.resetPlayer();
+            allEnemies[i].resetBug();
+            break;
+        }
+    }
+};
+
+// Now instantiate your objects.
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+// plays sound on only when left or right or down or up arrows are pressed.
+document.onkeydown = function(e) {
+    if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40)
+    document.getElementById('sound-one').play();
+};
